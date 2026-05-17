c4615119f15a14daf381b545f23a1130acc66ffd
Reem Zekry <reemhatemzekry@gmail.com>
2026-05-17 03:35:34 +0300

    Add inference script for model predictions

--- src/inference.py
+++ src/inference.py
@@ -1,1 +1,29 @@
+import pandas as pd
+import joblib
 
+from feature_engineering import feature_engineering
+
+# Load model
+model = joblib.load("models/model.pkl")
+
+# Load test data
+test_df = pd.read_csv("data/test.csv")
+
+test_df = feature_engineering(test_df)
+
+PassengerId = test_df["PassengerId"]
+
+X_test = test_df.drop(columns=["PassengerId"])
+
+# Predict
+preds = model.predict(X_test)
+
+# Submission
+submission = pd.DataFrame({
+    "PassengerId": PassengerId,
+    "Survived": preds.astype(int)
+})
+
+submission.to_csv("submissions/submission.csv", index=False)
+
+print("Submission file created!")
