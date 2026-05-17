246365bd4dc12fb9f1369c04ba36d605cc6ad2c8
Reem Zekry <reemhatemzekry@gmail.com>
2026-05-17 03:33:59 +0300

    Add preprocessing function for numeric and categorical data

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,1 +1,18 @@
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+from sklearn.impute import SimpleImputer
 
+def build_preprocessor(numeric_features, categorical_features):
+
+    numeric_transformer = StandardScaler()
+
+    categorical_transformer = OneHotEncoder(handle_unknown="ignore")
+
+    preprocessor = ColumnTransformer(
+        transformers=[
+            ("num", numeric_transformer, numeric_features),
+            ("cat", categorical_transformer, categorical_features)
+        ]
+    )
+
+    return preprocessor
